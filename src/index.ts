8f1f1ae5bf364d5002ab3f6137eab0ab1f5115b0
NadiaElso <92063948+NadiaElso@users.noreply.github.com>
2022-01-13 20:13:53 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+let precioinicial: number;
+let descuento: number;
+let preciofinal: number;
+
+precioinicial = 450;
+descuento = 0.1;
+preciofinal = precioinicial * descuento;
+
+console.log(preciofinal);
